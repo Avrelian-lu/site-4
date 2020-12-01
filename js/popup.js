8e8b05795038d2a24742baf5989757f6ce02da53
Avrelian-lu <71962309+Avrelian-lu@users.noreply.github.com>
2020-12-01 13:46:16 +0300

    site-4

---
+++ js/popup.js
@@ -0,0 +1,12 @@
+
+    function showsr(state) {
+        document.getElementById("search__form").style.display = state;
+        document.getElementById("gray-sr").style.display = state;
+      }
+
+
+      function showreg(state) {
+        document.getElementById("register__window").style.display = state;
+        document.getElementById("gray-reg").style.display = state;
+      }
+
